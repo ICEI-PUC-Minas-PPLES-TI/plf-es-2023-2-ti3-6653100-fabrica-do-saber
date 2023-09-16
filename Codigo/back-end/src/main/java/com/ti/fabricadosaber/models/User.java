5061e15a8d4f5af280c9f2540be017961ccfcf65
Marlene Moraes <marlenevmoraes@outlook.com>
2023-09-15 23:08:54 -0300

    backend code: add user model #3

--- Codigo/back-end/src/main/java/com/ti/fabricadosaber/models/User.java
+++ Codigo/back-end/src/main/java/com/ti/fabricadosaber/models/User.java
@@ -1,5 +1,38 @@
 package com.ti.fabricadosaber.models;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
-    
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+	@Column
+    private String fullName;
+	@Column
+    private String email;
+	@Column
+    private String password;
+	@Column
+    private LocalDate createDate;
+	
 }
