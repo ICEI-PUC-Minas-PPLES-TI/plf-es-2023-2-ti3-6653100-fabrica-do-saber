f2049386099161d2299ead708af6a6c6ed6ef221
Guilherme Costa <guilhermeldcosta@gmail.com>
2023-09-17 14:59:57 -0300

    front-end code: update student-list page #9

--- Codigo/front-end/src/app/components/pages/student-list/student-list.component.ts
+++ Codigo/front-end/src/app/components/pages/student-list/student-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Student} from '../../../interfaces/Student';
 
 @Component({
   selector: 'app-student-list',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./student-list.component.css']
 })
 export class StudentListComponent {
+
+  originalStudents: Student[] = [
+    {
+      name: 'Josefina da Silva',
+      contactNumber: 31984173698,
+      contactName: 'Paulinho',
+      age: 5,
+      birthDate: '20/04/2018',
+      class: 'Turma A',
+      status: 'Ativo'
+    },
+    {
+      name: 'Joao da Silva',
+      contactNumber: 31923198688,
+      contactName: 'Maria',
+      age: 4,
+      birthDate: '28/10/2020',
+      class: 'Turma B',
+      status: 'Ativo'
+    },
+    {
+      name: 'Carlitos da Silva',
+      contactNumber: 31923198688,
+      contactName: 'Maria',
+      age: 3,
+      birthDate: '28/10/2020',
+      class: 'Turma B',
+      status: 'Ativo'
+    },
+  ];
+  students = [...this.originalStudents];
+
+  filterStudentList(event: Event): void {
+    const searchInput = event.target as HTMLInputElement;
+    const inputValue = searchInput.value.toLowerCase();
+    this.students = this.originalStudents.filter((student: Student) => student.name.toLowerCase().includes(inputValue));
+  }
 
 }
---
+++ Codigo/front-end/src/app/interfaces/Student.ts
@@ -0,0 +1,9 @@
+export interface Student {
+  name: String,
+  contactNumber: number,
+  contactName: String,
+  age: number,
+  birthDate: String,
+  class: String,
+  status: String,
+}
