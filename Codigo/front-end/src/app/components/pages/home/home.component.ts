09a29d4b921103e60f44c35dac20da06d50e4a4c
Vitor Stahlberg <vitorlberg@gmail.com>
2023-09-20 22:17:30 -0300

    add: Home Page #11

--- Codigo/front-end/src/app/components/pages/home/home.component.ts
+++ Codigo/front-end/src/app/components/pages/home/home.component.ts
@@ -1,8 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent { }
+export class HomeComponent implements OnInit {
+
+  cards = [
+    {
+      title: 'Alunos',
+      imageUrl: '../../../assets/img/ger-alunos.png'
+    },
+    {
+      title: 'Funcionários',
+      imageUrl: '../../../assets/img/funcionarios.png'
+    },
+    {
+      title: 'Financeiro',
+      imageUrl: '../../../assets/img/financeiro.png'
+    },
+    {
+      title: 'Turmas',
+      imageUrl: '../../../assets/img/turmas.png'
+    },
+    {
+      title: 'Relatórios',
+      imageUrl: '../../../assets/img/relatorios.png'
+    },
+    {
+      title: 'Card 6',
+      imageUrl: ''
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
