bcd1a104b7810df33deb9b14c84c6343d7118d59
Marlene Moraes <marlenevmoraes@outlook.com>
2023-09-20 22:28:29 -0300

    front-end: add student form and title #12

--- Codigo/front-end/src/app/app.module.ts
+++ Codigo/front-end/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from "@angular/forms";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -12,7 +13,6 @@ import { LoginComponent } from './components/pages/login/login.component';
 import { StudentCreateComponent } from './components/pages/student-create/student-create.component';
 import { StudentEditComponent } from './components/pages/student-edit/student-edit.component';
 import { StudentListComponent } from './components/pages/student-list/student-list.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -29,7 +29,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    NgbModule
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Codigo/front-end/src/app/components/pages/student-create/student-create.component.ts
+++ Codigo/front-end/src/app/components/pages/student-create/student-create.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-student-create',
   templateUrl: './student-create.component.html',
   styleUrls: ['./student-create.component.css']
 })
-export class StudentCreateComponent implements OnInit {
+export class StudentCreateComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  states = [
+    'Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal',
+    'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul',
+    'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí',
+    'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia',
+    'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'
+  ];
 
 }
