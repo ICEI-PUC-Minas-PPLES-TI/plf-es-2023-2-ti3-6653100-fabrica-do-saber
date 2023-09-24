5379ccd748eefc968d7abd9c6fcaab77f0494c9e
Guilherme Costa <guilhermeldcosta@gmail.com>
2023-09-24 12:37:52 -0300

    front-end: btn component update

--- Codigo/front-end/src/app/components/forms/student-form/student-form.component.ts
+++ Codigo/front-end/src/app/components/forms/student-form/student-form.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input} from '@angular/core';
-import {Router} from "@angular/router";
-import {ToastrService} from "ngx-toastr";
-import {StudentService} from "../../../services/student.service";
+import {Router} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
+import {StudentService} from '../../../services/student.service';
 
 @Component({
   selector: 'app-student-form',
@@ -50,116 +50,7 @@ export class StudentFormComponent {
   @Input() motherCityOfResidence!: string;
   @Input() motherZipCode!: string;
 
-  constructor(private router: Router, private toastr: ToastrService, private studentService: StudentService) {
-  }
-
-  createStudent(): void {
-    // const student = {
-    //   guardians: [
-    //     {
-    //       fullName: this.fatherFullName,
-    //       cpf: this.fatherCpf,
-    //       rg: this.fatherRg,
-    //       email: this.fatherEmail,
-    //       occupation: this.fatherOccupation,
-    //       company: this.fatherCompany,
-    //       phoneNumber: this.fatherPhoneNumber,
-    //       streetAddress: this.fatherStreetAddress,
-    //       addressNumber: this.fatherAddressNumber,
-    //       neighborhood: this.fatherNeighborhood,
-    //       cityOfResidence: this.fatherCityOfResidence,
-    //       zipCode: this.fatherZipCode
-    //     },
-    //     {
-    //       fullName: this.motherFullName,
-    //       cpf: this.motherCpf,
-    //       rg: this.motherRg,
-    //       email: this.motherEmail,
-    //       occupation: this.motherOccupation,
-    //       company: this.motherCompany,
-    //       phoneNumber: this.motherPhoneNumber,
-    //       streetAddress: this.motherStreetAddress,
-    //       addressNumber: this.motherAddressNumber,
-    //       neighborhood: this.motherNeighborhood,
-    //       cityOfResidence: this.motherCityOfResidence,
-    //       zipCode: this.motherZipCode
-    //     }
-    //   ],
-    //   fullName: this.studentFullName,
-    //   registrationDate: '', // Preencha com o valor desejado
-    //   grade: this.studentGrade,
-    //   birthDate: this.studentBirthDate,
-    //   hometown: this.studentHometown,
-    //   homeState: this.studentHomeState,
-    //   nationality: this.studentNationality,
-    //   religion: this.studentReligion,
-    //   race: this.studentRace,
-    //   streetAddress: this.studentStreetAddress,
-    //   addressNumber: this.studentAddressNumber,
-    //   neighborhood: this.studentNeighborhood,
-    //   cityOfResidence: this.studentCityOfResidence,
-    //   zipCode: this.studentZipCode
-    // };
-
-    const student = {
-      guardians: [
-        {
-          fullName: "Nome Completo do Pai",
-          cpf: "123.456.789-00",
-          rg: "12.345.678",
-          email: "pai@email.com",
-          occupation: "Ocupação do Pai",
-          company: "Empresa do Pai",
-          phoneNumber: "11 98765-4321",
-          streetAddress: "Endereço do Pai",
-          addressNumber: "123",
-          neighborhood: "Bairro do Pai",
-          cityOfResidence: "Cidade de Residência do Pai",
-          zipCode: "12345-678"
-        },
-        {
-          fullName: "Nome Completo da Mãe",
-          cpf: "987.654.321-00",
-          rg: "11.222.333",
-          email: "mae@email.com",
-          occupation: "Ocupação da Mãe",
-          company: "Empresa da Mãe",
-          phoneNumber: "21 55555-1234",
-          streetAddress: "Endereço da Mãe",
-          addressNumber: "456",
-          neighborhood: "Bairro da Mãe",
-          cityOfResidence: "Cidade de Residência da Mãe",
-          zipCode: "54321-876"
-        }
-      ],
-      fullName: "Nome do Estudante",
-      registrationDate: "01/01/2023",
-      grade: "5º ano",
-      birthDate: "15/06/2010",
-      hometown: "Cidade Natal",
-      homeState: "Estado Natal",
-      nationality: "Brasileiro",
-      religion: "Religião do Estudante",
-      race: "Raça do Estudante",
-      streetAddress: "Endereço do Estudante",
-      addressNumber: "123",
-      neighborhood: "Bairro do Estudante",
-      cityOfResidence: "Cidade de Residência",
-      zipCode: "12345-678"
-    };
-
-    this.studentService.createStudent(student).subscribe(
-      response => {
-        console.log('Estudante criado com sucesso!', response);
-      },
-      error => {
-        console.error('Erro ao criar o estudante', error);
-      });
-  }
-
-  cancel() {
-    this.toastr.error('Ação Cancelada');
-    this.router.navigate(['/student-list']);
+  constructor() {
   }
 
   // todo: funcao de teste, deletar
--- Codigo/front-end/src/app/components/pages/student-create/student-create.component.ts
+++ Codigo/front-end/src/app/components/pages/student-create/student-create.component.ts
@@ -1,4 +1,7 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
+import {Router} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
+import {StudentService} from '../../../services/student.service';
 
 @Component({
   selector: 'app-student-create',
@@ -6,9 +9,71 @@ import {Component, ElementRef, ViewChild} from '@angular/core';
   styleUrls: ['./student-create.component.css']
 })
 export class StudentCreateComponent {
-  constructor() {
-  }
 
   @ViewChild('showAddressCheckbox') showAddressCheckbox!: ElementRef<HTMLInputElement>;
+  constructor(private router: Router, private toastr: ToastrService, private studentService: StudentService) {
+  }
 
+  createStudent(): void {
+
+    const student = {
+      guardians: [
+        {
+          fullName: 'Nome Completo do Pai',
+          cpf: '123.456.789-00',
+          rg: '12.345.678',
+          email: 'pai@email.com',
+          occupation: 'Ocupação do Pai',
+          company: 'Empresa do Pai',
+          phoneNumber: '11 98765-4321',
+          streetAddress: 'Endereço do Pai',
+          addressNumber: '123',
+          neighborhood: 'Bairro do Pai',
+          cityOfResidence: 'Cidade de Residência do Pai',
+          zipCode: '12345-678'
+        },
+        {
+          fullName: 'Nome Completo da Mãe',
+          cpf: '987.654.321-00',
+          rg: '11.222.333',
+          email: 'mae@email.com',
+          occupation: 'Ocupação da Mãe',
+          company: 'Empresa da Mãe',
+          phoneNumber: '21 55555-1234',
+          streetAddress: 'Endereço da Mãe',
+          addressNumber: '456',
+          neighborhood: 'Bairro da Mãe',
+          cityOfResidence: 'Cidade de Residência da Mãe',
+          zipCode: '54321-876'
+        }
+      ],
+      fullName: 'Nome do Estudante',
+      registrationDate: '01/01/2023',
+      grade: '5º ano',
+      birthDate: '15/06/2010',
+      hometown: 'Cidade Natal',
+      homeState: 'Estado Natal',
+      nationality: 'Brasileiro',
+      religion: 'Religião do Estudante',
+      race: 'Raça do Estudante',
+      streetAddress: 'Endereço do Estudante',
+      addressNumber: '123',
+      neighborhood: 'Bairro do Estudante',
+      cityOfResidence: 'Cidade de Residência',
+      zipCode: '12345-678'
+    };
+
+    this.studentService.createStudent(student).subscribe(
+      response => {
+        console.log('Estudante criado com sucesso!', response);
+      },
+      error => {
+        console.error('Erro ao criar o estudante', error);
+      });
+  }
+
+  cancel() {
+    this.toastr.error('Ação Cancelada');
+    this.router.navigate(['/student-list']);
+  }
 }
