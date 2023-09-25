3f56e6e47af6c2707f9a7602a3212ff477fa7b2a
Lucas Soares <polonio230@hotmail.com>
2023-09-25 15:14:46 -0300

    code: adicionar enumeration Race #56

--- Codigo/back-end/src/main/java/com/ti/fabricadosaber/enums/Race.java
+++ Codigo/back-end/src/main/java/com/ti/fabricadosaber/enums/Race.java
@@ -1,4 +1,23 @@
 package com.ti.fabricadosaber.enums;
 
 public enum Race {
+        AMARELO("Amarelo"),
+        BRANCO("Branco"),
+        INDIGENA("Indígena"),
+        PARDO("Pardo"),
+        PRETO("Preto"),
+        OUTRA("Outra"),
+        PREFIRO_NAO_DECLARAR("Prefiro não declarar");
+
+        private final String name;
+
+        Race(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    
+
 }
