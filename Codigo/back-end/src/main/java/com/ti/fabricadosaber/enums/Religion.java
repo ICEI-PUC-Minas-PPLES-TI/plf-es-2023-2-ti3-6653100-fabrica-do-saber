3e8057599e7dc669457f3111554bb71e8ab30a06
Lucas Soares <polonio230@hotmail.com>
2023-09-25 15:15:10 -0300

    code: adicionar enumeration religion #57

--- Codigo/back-end/src/main/java/com/ti/fabricadosaber/enums/Religion.java
+++ Codigo/back-end/src/main/java/com/ti/fabricadosaber/enums/Religion.java
@@ -1,4 +1,24 @@
 package com.ti.fabricadosaber.enums;
 
 public enum Religion {
+        CANDOMBLE("Candomblé"),
+        CATOLICISMO("Catolicismo"),
+        ESPIRITISMO("Espiritismo"),
+        PROTESTANTISMO("Protestantismo"),
+        UMBANDA("Umbanda"),
+        OUTRA("Outra"),
+        NAO_POSSUI("Não possui"),
+        PREFIRO_NAO_DECLARAR("Prefiro não declarar");
+
+        private final String name;
+
+        Religion(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+
 }
