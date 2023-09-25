dff04d53a64111266a3e0e6f9caaa28c33ace53e
Lucas Soares <polonio230@hotmail.com>
2023-09-25 15:15:59 -0300

    refactor: ajustar abreviações de State #55

--- Codigo/back-end/src/main/java/com/ti/fabricadosaber/enums/State.java
+++ Codigo/back-end/src/main/java/com/ti/fabricadosaber/enums/State.java
@@ -2,40 +2,41 @@ package com.ti.fabricadosaber.enums;
 
 public enum State {
 
-    ACRE("AC"),
-    ALAGOAS("AL"),
-    AMAPA("AP"),
-    AMAZONAS("AM"),
-    BAHIA("BA"),
-    CEARA("CE"),
-    DISTRITO_FEDERAL("DF"),
-    ESPIRITO_SANTO("ES"),
-    GOIAS("GO"),
-    MARANHAO("MA"),
-    MATO_GROSSO("MT"),
-    MATO_GROSSO_DO_SUL("MS"),
-    MINAS_GERAIS("MG"),
-    PARA("PA"),
-    PARAIBA("PB"),
-    PARANA("PR"),
-    PERNAMBUCO("PE"),
-    PIAUI("PI"),
-    RIO_DE_JANEIRO("RJ"),
-    RIO_GRANDE_DO_NORTE("RN"),
-    RIO_GRANDE_DO_SUL("RS"),
-    RONDONIA("RO"),
-    RORAIMA("RR"),
-    SANTA_CATARINA("SC"),
-    SAO_PAULO("SP"),
-    SERGIPE("SE"),
-    TOCANTINS("TO");
+    ACRE("Acre"),
+    ALAGOAS("Alagoas"),
+    AMAPA("Amapá"),
+    AMAZONAS("Amazonas"),
+    BAHIA("Bahia"),
+    CEARA("Ceará"),
+    DISTRITO_FEDERAL("Distrito Federal"),
+    ESPIRITO_SANTO("Espírito Santo"),
+    GOIAS("Goiás"),
+    MARANHAO("Maranhão"),
+    MATO_GROSSO("Mato Grosso"),
+    MATO_GROSSO_DO_SUL("Mato Grosso do Sul"),
+    MINAS_GERAIS("Minas Gerais"),
+    PARA("Pará"),
+    PARAIBA("Paraíba"),
+    PARANA("Paraná"),
+    PERNAMBUCO("Pernambuco"),
+    PIAUI("Piauí"),
+    RIO_DE_JANEIRO("Rio de Janeiro"),
+    RIO_GRANDE_DO_NORTE("Rio Grande do Norte"),
+    RIO_GRANDE_DO_SUL("Rio Grande do Sul"),
+    RONDONIA("Rondônia"),
+    RORAIMA("Roraima"),
+    SANTA_CATARINA("Santa Catarina"),
+    SAO_PAULO("São Paulo"),
+    SERGIPE("Sergipe"),
+    TOCANTINS("Tocantins");
 
-    private final String abbreviation;
+    private final String name;
 
-    State(String abbreviation) {
-        this.abbreviation = abbreviation;
+    State(String name) {
+        this.name = name;
     }
-    public String getAbbreviation() {
-        return abbreviation;
+
+    public String getName() {
+        return name;
     }
 }
