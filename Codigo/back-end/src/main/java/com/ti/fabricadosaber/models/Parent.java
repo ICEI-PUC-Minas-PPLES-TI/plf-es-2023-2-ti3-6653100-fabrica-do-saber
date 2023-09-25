4de84d773eb038811be242c45bb864b4edada1ee
Lucas Soares <polonio230@hotmail.com>
2023-09-25 14:56:14 -0300

    code: adicionar superclasse Parent #48

---
+++ Codigo/back-end/src/main/java/com/ti/fabricadosaber/models/Parent.java
@@ -0,0 +1,25 @@
+package com.ti.fabricadosaber.models;
+
+import com.ti.fabricadosaber.enums.State;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.MappedSuperclass;
+
+
+@MappedSuperclass
+public abstract class Parent extends Person {
+
+
+    @Column(name = "occupation", length = 45, nullable = false, updatable = true)
+    private String occupation;
+
+    @Column(name = "company", length = 45, nullable = false)
+    private String company;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "company", length = 2, nullable = false)
+    private State homeState;
+
+}
+
