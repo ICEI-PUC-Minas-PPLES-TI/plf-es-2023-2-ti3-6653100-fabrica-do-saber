dfe42264526df0caf350a4c8f0891624758ea66a
duda-amaral <dudaamaralmap3110@gmail.com>
2023-09-26 19:51:03 -0300

    code: add team repository and service #29

---
+++ Codigo/back-end/src/main/java/com/ti/fabricadosaber/services/TeamService.java
@@ -0,0 +1,62 @@
+package com.ti.fabricadosaber.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.ti.fabricadosaber.models.Student;
+import com.ti.fabricadosaber.models.Team;
+import com.ti.fabricadosaber.repositories.TeamRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class TeamService {
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    public Team findById(Long id) {
+        Optional<Team> team = this.teamRepository.findById(id);
+        return team.orElseThrow(() -> new RuntimeException(
+            "Turma não encontrada! Id: " + id + ", Tipo: " + Team.class.getName()));
+    }
+
+    public List<Team> listAllTeams() {
+        try {
+            return teamRepository.findAll();
+        } catch(EmptyResultDataAccessException error) {
+            throw new RuntimeException("Nenhuma turma cadastrada", error);
+        }
+    }
+
+    @Transactional
+    public Team create(Team obj) {
+        obj.setId(null);
+        obj = this.teamRepository.save(obj);
+        return obj;
+    }
+
+    public Team update(Team obj) {
+        Team newObj = findById(obj.getId());
+
+        BeanUtils.copyProperties(obj, newObj, "id");
+
+        return this.teamRepository.save(newObj);
+    }
+
+    public void delete(Long id) {
+        Team team = findById(id);
+
+        try {
+            this.teamRepository.delete(team);
+        } catch (Exception e) {
+            throw new RuntimeException("Não é possível excluir pois há entidades relacionadas");
+        }
+    }
+    
+}
