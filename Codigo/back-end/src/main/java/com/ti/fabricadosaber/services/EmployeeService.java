080918a3d60112cc1d806a68d69e72c1d0d1f6cd
duda-amaral <dudaamaralmap3110@gmail.com>
2023-09-26 17:02:57 -0300

    code: add employee service #26

---
+++ Codigo/back-end/src/main/java/com/ti/fabricadosaber/services/EmployeeService.java
@@ -0,0 +1,60 @@
+package com.ti.fabricadosaber.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.ti.fabricadosaber.models.Employee;
+import com.ti.fabricadosaber.repositories.EmployeeRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public Employee findById(Long id) {
+        Optional<Employee> employee = this.employeeRepository.findById(id);
+        return employee.orElseThrow(() -> new RuntimeException(
+                "Funcionário não encontrado! id: " + id + ", Tipo: " + Employee.class.getName()));
+    }
+
+    public List<Employee> listAllEmployees() {
+        try {
+            return this.employeeRepository.findAll();
+        } catch (EmptyResultDataAccessException error) {
+            throw new RuntimeException("Nenhum funcionário cadastrado", error);
+        }
+    }
+
+    @Transactional
+    public Employee create(Employee obj) {
+        obj.setId(null);
+        obj = this.employeeRepository.save(obj);
+        return obj;
+    }
+
+    public Employee update(Employee obj) {
+        Employee newObj = findById(obj.getId());
+
+        BeanUtils.copyProperties(obj, newObj, "id");
+
+        return this.employeeRepository.save(newObj);
+    }
+
+    public void delete(Long id) {
+        Employee employee = findById(id);
+
+        try {
+            this.employeeRepository.delete(employee);
+        } catch (Exception e) {
+            throw new RuntimeException("Não é possível excluir pois há entidades relacionadas");
+        }
+    }
+}
