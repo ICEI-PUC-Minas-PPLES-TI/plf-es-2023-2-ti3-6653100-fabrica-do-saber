f45a70cb5bb278c08a4d1ee8e2d6ff1b6e51a2b8
Lucas Soares <polonio230@hotmail.com>
2023-09-25 21:42:57 -0300

    code: adicionar model de Father #48

--- Codigo/back-end/src/main/java/com/ti/fabricadosaber/models/Father.java
+++ Codigo/back-end/src/main/java/com/ti/fabricadosaber/models/Father.java
@@ -1,10 +1,18 @@
 package com.ti.fabricadosaber.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.util.List;
 
 @Entity
+@Table(name = Father.TABLE_NAME)
 public class Father extends Parent {
 
+    public static final String TABLE_NAME = "father";
 
+    @OneToMany(mappedBy = "father")
+    private List<Student> children;
 
 }
