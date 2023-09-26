17f9b3760b5bbaa5fa4037169923b3b2adb675f2
Lucas Soares <polonio230@hotmail.com>
2023-09-25 21:43:44 -0300

    code: adicionar model de Mother #43

--- Codigo/back-end/src/main/java/com/ti/fabricadosaber/models/Mother.java
+++ Codigo/back-end/src/main/java/com/ti/fabricadosaber/models/Mother.java
@@ -1,10 +1,18 @@
 package com.ti.fabricadosaber.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.util.List;
 
 @Entity
+@Table(name = Mother.TABLE_NAME)
 public class Mother extends Parent {
 
+    public static final String TABLE_NAME = "mother";
+    @OneToMany(mappedBy = "mother")
+    private List<Student> children;
 
 
 }
