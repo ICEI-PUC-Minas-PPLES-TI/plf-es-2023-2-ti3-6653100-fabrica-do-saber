3d074402cd5eef53a1fa7119cd7e29fed1d5b420
Lucas Soares <polonio230@hotmail.com>
2023-09-25 21:46:29 -0300

    code: adicionar MotherRepository #46

---
+++ Codigo/back-end/src/main/java/com/ti/fabricadosaber/repositories/MotherRepository.java
@@ -0,0 +1,8 @@
+package com.ti.fabricadosaber.repositories;
+
+import com.ti.fabricadosaber.models.Mother;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MotherRepository extends JpaRepository<Mother, Long> {
+
+}
