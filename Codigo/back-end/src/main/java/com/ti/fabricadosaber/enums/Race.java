20f683a4e8a070dba514368c877e00a2ad5e7e6e
Lucas Soares <polonio230@hotmail.com>
2023-09-25 22:55:02 -0300

    refactor: mudar formato para JSON #56

--- Codigo/back-end/src/main/java/com/ti/fabricadosaber/enums/Race.java
+++ Codigo/back-end/src/main/java/com/ti/fabricadosaber/enums/Race.java
@@ -1,12 +1,21 @@
 package com.ti.fabricadosaber.enums;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public enum Race {
+        @JsonFormat(shape = JsonFormat.Shape.STRING)
         AMARELO("Amarelo"),
+        @JsonFormat(shape = JsonFormat.Shape.STRING)
         BRANCO("Branco"),
+        @JsonFormat(shape = JsonFormat.Shape.STRING)
         INDIGENA("Indígena"),
+        @JsonFormat(shape = JsonFormat.Shape.STRING)
         PARDO("Pardo"),
+        @JsonFormat(shape = JsonFormat.Shape.STRING)
         PRETO("Preto"),
+        @JsonFormat(shape = JsonFormat.Shape.STRING)
         OUTRA("Outra"),
+        @JsonFormat(shape = JsonFormat.Shape.STRING)
         PREFIRO_NAO_DECLARAR("Prefiro não declarar");
 
         private final String name;
