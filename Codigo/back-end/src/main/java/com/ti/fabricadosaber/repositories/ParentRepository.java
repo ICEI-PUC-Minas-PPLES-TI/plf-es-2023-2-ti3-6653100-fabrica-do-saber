13cfab309d6221de6441a8a07f0ed105575d5b35
Lucas Soares <polonio230@hotmail.com>
2023-09-26 23:51:34 -0300

    code: adicionar método findByCpf() e existsByCpf() #60

--- Codigo/back-end/src/main/java/com/ti/fabricadosaber/repositories/ParentRepository.java
+++ Codigo/back-end/src/main/java/com/ti/fabricadosaber/repositories/ParentRepository.java
@@ -4,4 +4,7 @@ import com.ti.fabricadosaber.models.Parent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ParentRepository extends JpaRepository<Parent, Long> {
+    boolean existsByCpf(String cpf);
+
+    Parent findByCpf(String cpf);
 }
