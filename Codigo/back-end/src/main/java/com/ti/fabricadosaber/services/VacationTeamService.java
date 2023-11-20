4db5da56f9b67c4afbdcc24b3bd519ebfb3f0c4f
Lucas Soares <polonio230@hotmail.com>
2023-11-20 20:02:16 -0300

    feat: add VacationTeamService

--- Codigo/back-end/src/main/java/com/ti/fabricadosaber/services/VacationTeamService.java
+++ Codigo/back-end/src/main/java/com/ti/fabricadosaber/services/VacationTeamService.java
@@ -1,243 +1,138 @@
 package com.ti.fabricadosaber.services;
 
-import java.time.LocalDate;
-import java.util.*;
-import java.util.stream.Collectors;
-
-import com.ti.fabricadosaber.components.StudentTeamOperation;
-import com.ti.fabricadosaber.dto.VacationTeamResponseDTO;
+import com.ti.fabricadosaber.exceptions.DataException;
 import com.ti.fabricadosaber.exceptions.EntityNotFoundException;
-import com.ti.fabricadosaber.exceptions.StudenteOnTeamException;
-import com.ti.fabricadosaber.services.exceptions.DataBindingViolationException;
+import com.ti.fabricadosaber.models.*;
+import com.ti.fabricadosaber.repositories.VacationTeamRepository;
+import com.ti.fabricadosaber.services.interfaces.TeamOperations;
 import com.ti.fabricadosaber.utils.SecurityUtil;
-import java.util.function.Predicate;
-
-import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
-
-import com.ti.fabricadosaber.models.Student;
-import com.ti.fabricadosaber.models.Teacher;
-import com.ti.fabricadosaber.models.Team;
-import com.ti.fabricadosaber.models.VacationTeam;
-import com.ti.fabricadosaber.repositories.VacationTeamRepository;
-import jakarta.transaction.Transactional;
+import java.time.LocalDate;
+import java.util.Set;
 
 @Service
-public class VacationTeamService {
+public class VacationTeamService implements TeamOperations {
 
     @Autowired
     private VacationTeamRepository vacationTeamRepository;
 
     @Autowired
-    private StudentTeamOperation studentTeamOperation;
-
-
-    @Autowired
     private TeacherService teacherService;
+
 
     @Autowired
     @Lazy
     private StudentService studentService;
 
+    @Autowired
+    @Lazy
+    private StudentTeamAssociationService studentTeamAssociationService;
 
+
+    @Override
     public VacationTeam findById(Long id) {
-        VacationTeam vacationTeam = this.vacationTeamRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(
-                "Creche de férias não encontrada! Id: " + id + ", Tipo: " + Team.class.getName()));
+        VacationTeam team = this.vacationTeamRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(
+                "Creche não encontrada! Id: " + id + ", Tipo: " + VacationTeam.class.getName()));
 
         SecurityUtil.checkUser();
-        return vacationTeam;
-    }
-
-    public List<VacationTeam> listAllVacationTeams() {
-        SecurityUtil.checkUser();
-
-        List<VacationTeam> team = this.vacationTeamRepository.findAll();
-        if (team.isEmpty()) {
-            throw new EntityNotFoundException("Nenhuma creche de férias cadastrada");
-        }
         return team;
     }
 
-    public Set<Student> listStudents(Long id) {
-        SecurityUtil.checkUser();
-
-        VacationTeam team = vacationTeamRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Creche de férias com o id " + id + " não encontrada."));
-
-        return team.getStudents();
+    public void checkDate(VacationTeam vacationTeam) {
+        dataNotNull(vacationTeam);
+        endDateValidate(vacationTeam);
+        startDateValidate(vacationTeam);
+        validateDateRange(vacationTeam);
     }
 
-    @Transactional
     public VacationTeam create(VacationTeam obj) {
-        Teacher teacher = this.teacherService.findById(obj.getTeacher().getId());
+
         obj.setId(null);
-        obj.setTeacher(teacher);
-        this.processStudentInCreation(obj);
-        obj.setStartDate(LocalDate.now());
+        obj.setTeacher(checkTeacher(obj.getTeacher()));
+        checkDate(obj); //validar a data
+
         obj = this.vacationTeamRepository.save(obj);
-        //this.associateStudents(obj);
+
+        teamStudents(obj);
+
+
         return obj;
+
     }
 
-    @Transactional
-    public VacationTeam save(VacationTeam vacationTeam) {
-        return vacationTeamRepository.save(vacationTeam);
-    }
-
-   /* public void associateStudents(VacationTeam obj) {
-        if (obj.getStudents() != null) {
-            for (Student student : obj.getStudents()) {
-                student.setTeam(obj);
-            }
-        }
-    }*/
-
-
-    public void removeStudentFromAllTeams(Student student) {
-        List<VacationTeam> teams = vacationTeamRepository.findByStudentsContaining(student);
-        for (VacationTeam team : teams) {
-            team.getStudents().remove(student);
-            updateVacationTeamStudentCount(team);
-        }
-    }
-
-
-
-
-    public void updateVacationTeamStudentCount(VacationTeam vacationTeam) {
-        int studentCount = vacationTeam.getStudents().size();
-        vacationTeam.setNumberStudents(studentCount);
-    }
-
-    public void processStudentInCreation(VacationTeam obj) {
-        Set<Student> students = obj.getStudents();
-        obj.setNumberStudents(0);
-
-        if (students != null && !students.isEmpty()) {
-            Set<Student> updatedStudents = new HashSet<>();
-
-            for (Student student : students) {
-
-                Student existingStudent = studentService.findById(student.getId());
-
-                existingStudent.getVacationTeams().add(obj);
-                updatedStudents.add(existingStudent);
-            }
-
-            obj.setNumberStudents(updatedStudents.size());
-            obj.setStudents(updatedStudents);
-
-        }
-    }
-
-
-
-    @Transactional
     public VacationTeam update(VacationTeam obj) {
-        VacationTeam newObj = findById(obj.getId());
-        newObj.setName(obj.getName());
-        newObj.setClassroom(obj.getClassroom());
-        newObj.setGrade(obj.getGrade());
-        newObj.setTeacher(obj.getTeacher());
-        newObj.setStartDate(obj.getStartDate());
-        newObj.setEndDate(obj.getEndDate());
+        return null;
+    }
 
-        processStudentOnUpdate(obj, newObj);
+    public void teamStudents(VacationTeam obj) {
 
+        Set<Long> studentIds = obj.getStudentIds();
 
-        newObj.setNumberStudents(obj.getNumberStudents());
-        newObj.setStudents(obj.getStudents());
+        if(studentIds != null && !studentIds.isEmpty()) {
 
+            for(Long studentId : studentIds) {
+                Student existingStudent = studentService.findById(studentId);
 
-        newObj = this.vacationTeamRepository.save(newObj);
-        //studentTeamOperation.associateStudents(newObj);
-        return newObj;
+                studentTeamAssociationService.processStudentInTeam(new StudentTeamAssociation(existingStudent, obj));
+            }
+        }
     }
 
 
+    @Override
+    public Teacher checkTeacher(Teacher teacher) {
+        Teacher existingTeacher = null;
+
+        if(teacher != null)
+            existingTeacher = teacherService.findById(teacher.getId());
+
+        return existingTeacher;
+    }
+
+    public static void validateDateRange(VacationTeam vacationTeam) {
+        LocalDate dataInicio = vacationTeam.getStartDate();
+        LocalDate dataTermino = vacationTeam.getEndDate();
+
+        if (dataInicio.isAfter(dataTermino)) {
+            throw new DataException("A data de início não pode ser posterior à data de término.");
+        }
+    }
+
+    public void dataNotNull(VacationTeam vacationTeam) {
+        LocalDate dataInicio = vacationTeam.getStartDate();
+        LocalDate dataTermino = vacationTeam.getEndDate();
+
+        if(dataInicio == null || dataTermino == null) {
+            throw new DataException("A data de início e término da creche de férias não pode ser nulo.");
+        }
+    }
+
+
+    public void startDateValidate(VacationTeam vacationTeam) {
+        if(!(vacationTeam.getStartDate().isAfter(LocalDate.now())))
+            throw new DataException("A creche só pode começar de hoje em diante!");
+    }
+
+    public void endDateValidate(VacationTeam vacationTeam) {
+        if (LocalDate.now().isAfter(vacationTeam.getEndDate()))
+            throw new DataException("A data de término não pode ser antes de hoje!");
+    }
+
+
+
+    public void updateTeamStudentCount(VacationTeam vacationTeam, Integer studentCount) {
+        vacationTeam.setNumberStudents(studentCount);
+        vacationTeamRepository.save(vacationTeam);
+    }
+
+
+    @Override
     public void delete(Long id) {
-        VacationTeam team = findById(id);
-        try {
-            this.vacationTeamRepository.delete(team);
-        } catch (Exception e) {
-            throw new DataBindingViolationException("Não é possível excluir pois há entidades relacionadas");
-        }
-    }
-
-    public void processStudentOnUpdate(VacationTeam obj, VacationTeam newObj) {
-        processStudentInCreation(obj);
-        Set<Student> students = newObj.getStudents();
-
-        if(students != null) {
-            if(obj.getNumberStudents() != 0) {
-                for (Student student : students) {
-                    if (!obj.getStudents().contains(student))
-                        student.setTeam(null);
-                }
-            } else {
-                Predicate<Student> alwaysTrue = student -> {
-                    student.setTeam(null);
-                    return true;
-                };
-                students.removeIf(alwaysTrue);
-            }
-        }
 
     }
 
 
 
-    public void updateStudent(Student student) {
-        //VacationTeam team = student.getTeam();
-        // if (team != null) {
-        //     student.getTeam().getStudents().remove(student);
-        //     student.getTeam().setNumberStudents(student.getTeam().getStudents().size());
-        //     vacationTeamRepository.save(team);
-        // }
-    }
-
-
-    // O controller acessa esse método
-    public VacationTeam deleteStudentFromTeam(Long teamId, List<Long> idsStudent) {
-        VacationTeam team = findById(teamId);
-
-        for (Long idStudent : idsStudent) {
-
-            Student student = studentService.findById(idStudent);
-            if (!(team.getStudents().contains(student))) {
-                throw new StudenteOnTeamException("Aluno não está vinculado a creche de férias" + team.getName());
-            }
-
-            updateStudent(student);
-            student.setTeam(null);
-            team.getStudents().remove(student);
-        }
-
-        updateVacationTeamStudentCount(team);
-        return team;
-    }
-
-    public VacationTeamResponseDTO convertToVacationTeamResponseDTO(VacationTeam team) {
-
-        VacationTeamResponseDTO dto = new VacationTeamResponseDTO();
-        dto.setId(team.getId());
-        dto.setName(team.getName());
-        dto.setClassroom(team.getClassroom());
-        dto.setGrade(team.getGrade());
-        dto.setNumberStudents(team.getNumberStudents());
-        dto.setTeacherId(team.getTeacher().getId());
-        dto.setStartDate(team.getStartDate());
-        dto.setEndDate(team.getEndDate());
-
-        if (team.getStudents() != null) {
-            List<Long> studentIds = team.getStudents().stream()
-                    .map(Student::getId)
-                    .collect(Collectors.toList());
-            dto.setStudentIds(studentIds);
-        }
-
-        return dto;
-    }
 }
