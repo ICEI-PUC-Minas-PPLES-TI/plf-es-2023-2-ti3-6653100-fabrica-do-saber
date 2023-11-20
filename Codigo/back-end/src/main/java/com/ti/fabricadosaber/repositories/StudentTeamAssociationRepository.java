02497b2c0e3d1c6118bb0e51ae2110ee2a8e9f8f
Lucas Soares <polonio230@hotmail.com>
2023-11-20 19:58:41 -0300

    feat: add StudentTeamAssociationRepository

---
+++ Codigo/back-end/src/main/java/com/ti/fabricadosaber/repositories/StudentTeamAssociationRepository.java
@@ -0,0 +1,34 @@
+package com.ti.fabricadosaber.repositories;
+
+import com.ti.fabricadosaber.models.StudentTeamAssociation;
+import com.ti.fabricadosaber.models.Team;
+import com.ti.fabricadosaber.models.VacationTeam;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface StudentTeamAssociationRepository extends CrudRepository<StudentTeamAssociation,
+        StudentTeamAssociation.StudentTeamId> {
+
+    boolean existsByStudentIdAndTeamId(Long studentId, Long teamId);
+
+    boolean existsByStudentIdAndTeamIdAndIsActiveTrue(Long studentId, Long teamId);
+
+    List<StudentTeamAssociation> findByTeamAndIsActive(Team team, boolean isActive);
+    
+
+    @Query("SELECT sta FROM StudentTeamAssociation sta JOIN Team t ON sta.id.teamId = t.id WHERE sta.id.studentId = :studentId AND TYPE(t) = Team AND sta.isActive = true")
+    Optional<StudentTeamAssociation> findFirstTeamByStudentIdAndIsActiveIsTrue(@Param("studentId") Long studentId);
+
+
+    Integer countByTeamAndIsActiveIsTrue(VacationTeam vacationTeam);
+
+    Integer countByTeamAndIsActiveIsTrue(Team team);
+}
+
+
