54aaf6f421577a117c691e957ece7ffd033a6a3b
Lucas Soares <polonio230@hotmail.com>
2023-11-27 09:00:44 -0300

    feat: listar todas as creche de férias

--- Codigo/back-end/src/main/java/com/ti/fabricadosaber/repositories/VacationTeamRepository.java
+++ Codigo/back-end/src/main/java/com/ti/fabricadosaber/repositories/VacationTeamRepository.java
@@ -1,7 +1,9 @@
 package com.ti.fabricadosaber.repositories;
 
 import com.ti.fabricadosaber.models.Student;
+import com.ti.fabricadosaber.models.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ti.fabricadosaber.models.VacationTeam;
@@ -10,5 +12,9 @@ import java.util.List;
 
 @Repository
 public interface VacationTeamRepository extends JpaRepository<VacationTeam, Long> {
+
+    @Query("SELECT t FROM Team t WHERE TYPE(t) = VacationTeam ")
+    List<VacationTeam> findAllTeams();
+
 
 }
