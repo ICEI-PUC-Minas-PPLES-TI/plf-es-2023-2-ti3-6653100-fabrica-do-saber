0c40ad23bf7e01eca55dd866a6a96a54d4904837
Lucas Soares <polonio230@hotmail.com>
2023-11-30 14:09:02 -0300

    refactor: remover  import de preAuthorize

--- Codigo/back-end/src/main/java/com/ti/fabricadosaber/controllers/StudentController.java
+++ Codigo/back-end/src/main/java/com/ti/fabricadosaber/controllers/StudentController.java
@@ -12,7 +12,7 @@ import com.ti.fabricadosaber.models.Parent;
 import com.ti.fabricadosaber.models.VacationTeam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
